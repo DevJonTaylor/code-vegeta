a1c964f2d940b80bea7f17b12a7f94bec39da26a
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-05-24 12:25:27 -0500

    fix styling bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 import poweredBy from "./powered-by-vitawind-dark.png";
 import Navbar from "./components/Navbar/Navbar";
-import "grapesjs/dist/css/grapes.min.css";
-import grapesjs from "grapesjs";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="text-center selection:bg-green-900">
-      <header className="flex min-h-screen flex-col items-center justify-between bg-[#282c34] text-white">
+      <header>
         <Navbar />
       </header>
 
-      <main>
+      <main className="flex min-h-screen flex-col items-center justify-between bg-[#282c34] text-white">
         <p className="bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent">
           VEGETA
         </p>
