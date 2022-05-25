102c53cd8c6a67305865826d446fa356ec847ca0
jonathanprill <jonathantprill@gmail.com>
2022-05-25 17:33:31 -0500

    fixes main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Landing from "./pages/Landing"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Landing />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
