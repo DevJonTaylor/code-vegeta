315b5e4424c4cc2c3f8544a49314574434c29952
jonathanprill <jonathantprill@gmail.com>
2022-05-26 13:59:58 -0500

    adds moon to landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -11,19 +11,27 @@ const Landing = () => {
             <header>
                 <Navbar />
             </header>
+            <div className='background-image'>
+                <div className='moon-border'>
+                    <div className='scrolling-image'></div>
+                </div>
+            </div>
+
+
             <div className='landing-page'>
-                <div className='welcome-text'>
-                    <h2>CODE YOUR OWN WEBSITE</h2>
-                    <h1>WITHOUT A KEYBOARD</h1>
+                <div className='welcome-text-left'>
+                    <h2 className='landing-subheader'>CODE YOUR OWN WEBSITE</h2>
+                    <h1 className='landing-header'>WITHOUT A KEYBOARD</h1>
                     <p>Build your online presence with Vegeta.</p>
+                </div>
+                <div className='spacer-border'></div>
+                <div className='welcome-text-right'>
+                    
                     <button className="btn welcome-btn">
                         Start Building
                     </button>
                 </div>
-                <div className='site-image-container'>
-                    <div className='welcome-image'><div className='welcome-gif'></div></div>
-                    
-                </div>
+
             </div>
         </main>
     );
