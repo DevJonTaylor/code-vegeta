623e450957a97dc8c9a6d9b8ef05610ef6e4c182
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-05-31 14:00:08 -0500

    refactor footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Navbar from "./components/Navbar/Navbar";
+import Footer from './components/Footer/Footer';
 import poweredBy from '../powered-by-vitawind-bright.png'
 import { Outlet } from "react-router-dom";
 
@@ -28,43 +29,7 @@ function App() {
           </p> */}
       </main>
 
-      {/* ADDED bg-black TO FOOTER */}
-      <footer className="flex w-full items-center justify-around bg-black">
-        <div className="w-48">
-          <p>
-            Built with <span alt="sweat">💦</span> by
-            <br />
-            <code className="app-text text-[#8d96a7]">
-              Jess, Jonathan, JT, Jordan, and Keenan
-            </code>
-            {""}
-          </p>
-        </div>
-        <div className="w-49 hidden sm:flex">
-          <p className="mt-3 flex gap-3 text-center text-[#8d96a7]">
-            <a
-              className="text-[#61dafb] transition-all hover:text-blue-400"
-              href="https://github.com/DevJonTaylor/code-vegeta"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github
-            </a>
-            {" | "}
-            <a
-              className="text-[#61dafb] transition-all hover:text-blue-400"
-              href="/donate"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Donate
-            </a>
-          </p>
-        </div>
-        <div className="w-49 hidden sm:flex">
-          <img src={poweredBy} className="mx-auto my-5" alt="powered-by" />
-        </div>
-      </footer>
+     <Footer />
     </div>
   );
 }
