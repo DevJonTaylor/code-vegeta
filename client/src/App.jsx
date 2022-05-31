db5865c5e31695bc3336218d5d615cc8d93f2b7f
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-05-31 14:17:25 -0500

    remove unnecessary png import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import poweredBy from "../powered-by-vitawind-bright.png";
 import { Outlet } from "react-router-dom";
 
 function App() {
