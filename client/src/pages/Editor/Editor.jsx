044fa44423cdbe250acc87c232340cdd6594743b
jonathanprill <jonathantprill@gmail.com>
2022-05-31 13:46:35 -0500

    adds save button

--- client/src/pages/Editor/Editor.jsx
+++ client/src/pages/Editor/Editor.jsx
@@ -149,19 +149,19 @@ class Editor extends React.Component {
                   "data-tip": "View Code",
                 },
               },
-              {
-                id: "vegeta",
-                className: "sidebar-icon group",
-                label: `
-                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                      <path fill="currentColor" d="M17 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.89 21 5 21H19C20.1 21 21 20.1 21 19V7L17 3M19 19H5V5H16.17L19 7.83V19M12 12C10.34 12 9 13.34 9 15S10.34 18 12 18 15 16.66 15 15 13.66 12 12 12M6 6H15V10H6V6Z" />
-                    </svg>
-                `,
-                command: "vegeta",
-                attributes: {
-                  "data-tip": "Save to database",
-                },
-              },
+              // {
+              //   id: "vegeta",
+              //   className: "sidebar-icon group",
+              //   label: `
+              //       <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+              //         <path fill="currentColor" d="M17 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.89 21 5 21H19C20.1 21 21 20.1 21 19V7L17 3M19 19H5V5H16.17L19 7.83V19M12 12C10.34 12 9 13.34 9 15S10.34 18 12 18 15 16.66 15 15 13.66 12 12 12M6 6H15V10H6V6Z" />
+              //       </svg>
+              //   `,
+              //   command: "vegeta",
+              //   attributes: {
+              //     "data-tip": "Save to database",
+              //   },
+              // },
               {
                 id: "preview",
                 className: "sidebar-icon group",
