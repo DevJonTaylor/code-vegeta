53bfb34341afd477f55891d1cb4d04d180c3c3b2
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-05-31 14:14:40 -0500

    clean up App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,21 +10,8 @@ function App() {
     <div className="flex flex-col text-center selection:bg-green-900">
       <Navbar />
 
-      <main
-        // className="grow bg-[#282c34] text-white"  DO WE NEED THIS AS A DIFF COLOR? SORRY FOR CHANGING IF SO - FEEL FREE TO CHANGE BACK
-        className="grow bg-neutral text-neutral"
-        style={{ marginBottom: "0px" }}
-      >
+      <main>
         <Outlet />
-        {/* <p className="mt-3">
-            <button
-              type="button"
-              className="my-6 rounded bg-gray-300 px-2 py-2 text-[#282C34] transition-all hover:bg-gray-200"
-              onClick={() => setCount((count) => count + 1)}
-            >
-              count is: {count}
-            </button>
-          </p> */}
       </main>
 
       <Footer />
