96e39275f6ef42ebf032a897773c4ca70e620c11
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-06-01 18:34:22 -0500

    seeding

--- server/seeds/user-seeder.js
+++ server/seeds/user-seeder.js
@@ -2,15 +2,17 @@ var User = require('../models/User');
 
 var mongoose = require('mongoose');
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/code-vegeta',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-);
+const dev = require("../config/connection");
+
+// mongoose.connect(
+//     process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/code-vegeta',
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false
+//     }
+// );
   
 var users = [
     new User({
@@ -29,17 +31,39 @@ var users = [
     })
 ];
 
-var done = 0;
+//connect mongoose
+mongoose
+  .connect(String(dev.db), { useNewUrlParser: true })
+  .catch(err => {
+    console.log(err.stack);
+    process.exit(1);
+  })
+  .then(() => {
+    console.log("connected to db in development environment");
+  });
 
-for (var i = 0; i < users.length; i++) {
-    users[i].save( function(err, result) {
-        done++;
-        if (done === users.length) {
-            exit();
-        }
-    });
-}
+//save your data. this is an async operation
+//after you make sure you seeded all the products, disconnect automatically
+users.map(async (p, index) => {
+  await p.save((err, result) => {
+    if (index === products.length - 1) {
+      console.log("DONE!");
+      mongoose.disconnect();
+    }
+  });
+});
 
-function exit() {
-    mongoose.disconnect();
-}
+// var done = 0;
+
+// for (var i = 0; i < users.length; i++) {
+//     users[i].save( function(err, result) {
+//         done++;
+//         if (done === users.length) {
+//             exit();
+//         }
+//     });
+// }
+
+// function exit() {
+//     mongoose.disconnect();
+// }
