039580fa3077447e6872885690c49ef517de7c32
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-05-31 19:48:54 -0500

    button changes

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -85,7 +85,10 @@ const Login = () => {
                                     value={formState.password}
                                     onChange={handleChange}
                                 />
-                                <button className='btn-login d-block rounded-full btn-main shadow-lg' type='submit'>
+                                {/* <button className='btn-login d-block rounded-full btn-main shadow-lg' type='submit'>
+                                    Sign In
+                                </button> */}
+                                <button className='btn-login d-block rounded-full btn-main shadow-lg text-primary bg-neutral' type='submit'>
                                     Sign In
                                 </button>
                                 <p className='end font-bold'>Don't have an account? <Link to="/signup" className='text-black font-bold'>Sign Up</Link></p>
