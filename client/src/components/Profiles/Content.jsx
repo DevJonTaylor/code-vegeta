145aae2f264fe3a2401bd33bf7006634338bc0e6
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-06-01 16:14:00 -0500

    displaying number of pages on profile

--- client/src/components/Profiles/Content.jsx
+++ client/src/components/Profiles/Content.jsx
@@ -90,7 +90,7 @@ const Content = ({ friendCount, username, friends, userParam, user_id, pages }) 
                                 <div className="flex justify-center py-4 lg:pt-4 pt-8">
                                     <div className="mr-4 p-3 text-center">
                                         <span className="text-xl font-bold block uppercase tracking-wide text-gray-900">
-                                            22
+                                            {pages.length}
                                         </span>
                                         <span className="text-sm text-gray-700 font-bold">
                                             Templates
