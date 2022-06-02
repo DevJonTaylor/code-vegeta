fee24150bd2d3744a030e2e847a25fad7526c249
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-06-01 21:44:24 -0500

    try relative paths

--- server/server.js
+++ server/server.js
@@ -55,9 +55,9 @@ app
 // }
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('dist'));
+  app.use(express.static('../client/dist'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join('dist', 'index.html'));
+    res.sendFile(path.join('../client/dist', '../client/dist/index.html'));
   });
 }
 
